4ef075e01bbed25f5739d5fe46f0807bcb7d8f47
Faithful Okere <feitful20@gmail.com>
2020-05-19 17:22:35 -0400

    updated routes

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -7,9 +7,7 @@ const path = require("path");
 module.exports = function (app) {
 
     //If no matching route is found, default to index.
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+
 
     //HTML GET requests
     //The code below handles when users visit a page. In each case below, the user is shown an HTML page of content
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Npm packages inorder to give our server useful functionality
 const express = require("express");
+const path = require("path");
 
 const app = express(); //This tells node that we are creating an express server
 const PORT = process.env.PORT || 8080; //Sets initial port
@@ -11,7 +12,9 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static("public"));
 
-
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 //Router
 //The below points our server to a series of route files.
